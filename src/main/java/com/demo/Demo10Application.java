e8d63e50ef6d559f3309e37478ad266675425e21
zhibo <zhibolian@quarkfinance.com>
2018-08-15 10:29:43 +0800

    demo08

---
+++ src/main/java/com/demo/Demo10Application.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo10Application.class, args);
+	}
+}
---
+++ src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public  String Hello(){
+        return "this is pipeline";
+    }
+}
